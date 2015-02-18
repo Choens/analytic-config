c60ca294e89255a41a7224f4066570bb7572f757
Andy Choens <andy.choens@gmail.com>
2015-02-18 10:42:07 -0500

    Major update to the packages.R file.
    Syncs the packages.R file with analytic-template-r. This is a
    significant improvement in the way this works. Also adds a few new
    packages. This is more or less, a full rewrite.

--- R/packages.R
+++ R/packages.R
@@ -1,43 +1,25 @@
 ## #############################################################################
 ## Packages
 ##
-## Useful list of packages everyone should have.
+## Collection of recommended packages to install.
+##
+## - Loads packages. Will attempt to install any / all missing packages.
+##
+## You can find these packages on CRAN:
+## http://cran.r-project.org/
 ##
 ## #############################################################################
 
 
 
-## =============================================================================
-## UPDATE PACKAGES
-##
-## Before trying to install any additional packages from CRAN, make sure
-## everything that is already installed is up to date.
-##
-## =============================================================================
-
 update.packages()
 
-## =============================================================================
-## LOAD AND ATTACH ADD ON PACKAGES
-##
-## - Uncomment required packages. Feel free to add packages not included here.
-##   This is just a convenient sample of popular packages.
-## - Missing packages will be installed.
-## - First package cannot begin with a ",".
-##
-## For more information, you can find these packages on CRAN. To use the
-## following URL, replace package-name with the name of the package name as
-## listed below:
-## http://cran.r-project.org/web/packages/package-name
-## 
-## =============================================================================
-
-pkgs <- c(
+packages <- c(
     ## -------------------------------------------------------------------------
-    ## R-Recommended - packages that are recommended by the upstream R core 
+    ## R-Recommended - packages that are recommended by the upstream R core
     ## team as part of a complete R installation.
     ## -------------------------------------------------------------------------
-    "boot"           ## Bootstrap R (S-Plus) Functions from the book 
+    ,"boot"           ## Bootstrap R (S-Plus) Functions from the book
                       ## "Bootstrap Methods and Their Applications" by A.C.
                       ## Davison and D.V. Hinkley (1997).
     ,"cluster"        ## Functions for clustering (by Rousseeuw et al.)
@@ -55,7 +37,7 @@ pkgs <- c(
     ,"nlme"           ## Linear and nonlinear mixed effects models.
     ,"rpart"          ## Recursive partitioning and regression trees.
     ,"survival"       ## Survival analysis, including penalised likelihood.
-    ##
+    
     ## -------------------------------------------------------------------------
     ## Collection of other popular packages.
     ## -------------------------------------------------------------------------
@@ -97,7 +79,7 @@ pkgs <- c(
     ,"gmodels"        ## Various R programming tools for model fitting.
     ,"googleVis"      ## Interface between R and Google Charts. Provides an
                       ## interface between R and the Google Charts API.
-    ,"gplots"         ## Various R programming tools for plotting data. 
+    ,"gplots"         ## Various R programming tools for plotting data.
     ,"gtools"         ## Various R programming tools.
     ,"Hmisc"          ## Many functions useful for data analysis, high-level
                       ## graphics, utility operations, functions for
@@ -114,7 +96,7 @@ pkgs <- c(
                       ## statistical data and the web. The package offers
                       ## flexible, robust, high performance tools for working
                       ## with JSON in R and is particularly powerful for
-                      ## building pipelines and interacting with web APIs. 
+                      ## building pipelines and interacting with web APIs.
     ,"lme4"           ## Fit linear and generalized linear mixed-effects
                       ## models. The models and their components are
                       ## represented using S4 classes and methods.
@@ -144,12 +126,23 @@ pkgs <- c(
                       ## demos reproducing analyzes presented in the book
                       ## "Multiple Comparisons Using R" (Bretz, Hothorn,
                       ## Westfall, 2010, CRC Press).
+    ,"pander"         ## Contains some functions catching all messages,
+                      ## stdout and other useful information while evaluating
+                      ## R code and other helpers to return user specified
+                      ## text elements (like: header, paragraph, table, image,
+                      ## lists etc.) in Pandoc's markdown or several type of R
+                      ## objects similarly automatically transformed to
+                      ## markdown format. Also capable of exporting/converting
+                      ## (the resulting) complex Pandoc documents to e.g.
+                      ## HTML, PDF, docx or odt. This latter reporting feature
+                      ## is supported in brew syntax or with a custom
+                      ## reference class with a smarty caching backend.
     ,"plyr"           ## A set of tools that solves a common set of problems:
                       ## you need to break a big problem down into manageable
                       ## pieces, operate on each pieces and then put all the
                       ## pieces back together.
     ,"psych"          ## A number of routines for personality, psychometrics
-                      ## and experimental psychology. 
+                      ## and experimental psychology.
     ,"randomForest"   ## Classification and regression based on a forest of
                       ## trees using random inputs.
     ,"reshape2"       ## Reshape lets you flexibly restructure and aggregate
@@ -172,12 +165,12 @@ pkgs <- c(
     ,"sp"             ## A package that provides classes and methods for
                       ## spatial data.
                       ## The classes document where the spatial location
-                      ## information resides, for 2D or 3D data. 
+                      ## information resides, for 2D or 3D data.
     ,"sqlutils"       ## This package provides utilities for working with a
                       ## library of SQL files.
     ,"stringr"        ## stringr is a set of simple wrappers that make R's
                       ## string functions more consistent, simpler and easier
-                      ## to use. It does this by ensuring that: function and 
+                      ## to use. It does this by ensuring that: function and
                       ## argument names (and positions) are consistent, all
                       ## functions deal with NA's and zero length character
                       ## appropriately, and the output data structures from
@@ -189,7 +182,7 @@ pkgs <- c(
     ,"vcd"            ## Visualization techniques, data sets, summary and
                       ## inference procedures aimed particularly at
                       ## categorical data. Special emphasis is given to
-                      ## highly extensible grid graphics. The package was 
+                      ## highly extensible grid graphics. The package was
                       ## inspired by the book "Visualizing Categorical Data"
                       ## by Michael Friendly.
     ,"XLConnect"      ## Manipulate Excel files from R.
@@ -208,20 +201,79 @@ pkgs <- c(
                         ## time series of numeric vectors/matrices and factors.
     )
 
-## Attempts to install missing packages ----------------------------------------
-library(utils)
-miss.pkgs <- pkgs[! pkgs %in% rownames(installed.packages())]
-if( length(miss.pkgs) > 0 ) {
-    print( "Installing the following packages:" )
-    print( paste("  - ", miss.pkgs) )
-    print( "This may take a few minutes to complete." )
-    install.packages( miss.pkgs, dependencies = TRUE )
+if(!exists("packages")) {
+    stop("The packages vector from the packages.R file does not exist")
 }
 
-## Loads packages, or errors out and stops ------------------------------------
-miss.pkgs <- pkgs[! pkgs %in% rownames(installed.packages())]
-if(length(miss.pkgs) == 0 ) {
-    lapply(pkgs, library, character.only=T)
-} else {
-    stop("Unable to install all needed packages.")
+#' Function to load a vector of packages. If a package is missing, it tries
+#' to install the package for you if you set install_missing to TRUE.
+#'
+#' @param packages List of packages to install.
+#' @param install_missing Must be either TRUE or FALSE. If TRUE, load_packages
+#' will try to install all missing packages. TRUE is the default value. If
+#' FALSE, load_packages will try to load the package and return a warning to
+#' stderr and will return 5.
+#'
+#' @return 1 or 5. If a 1 is returned, all went well. If 5 is returned, there
+#' was an error.
+#'
+load_packages = function(packages, install_missing = TRUE) {
+
+    if( length(packages) == 0 ) return(1)
+
+    ## Identifies missing packages ---------------------------------------------
+    missing_packages <- packages[!packages %in% rownames(installed.packages())]
+
+    ## Attempts to install missing packages ----------------------------------------
+    if( length(missing_packages) > 0 & install_missing == TRUE ) {
+        warning("You are missing packages from packages.")
+        warning("pdating current packages before installing new packages.")
+        update.packages(ask=FALSE)
+        message( "Installing the following packages:" )
+        message( missing_packages)
+        message( "This may take several minutes to complete." )
+        install.packages( missing_packages, dependencies = TRUE )
+    }
+    ## Identifies missing packages ---------------------------------------------
+    missing_packages <- packages[! packages %in% rownames(installed.packages())]
+
+
+    ## Loads packages, or returns some warnings and 5. -------------------------
+    if(length(missing_packages) == 0 || install_missing == FALSE ) {
+
+        load_success = rep(FALSE, length(packages))
+
+        for(i in 1:length(packages)) {
+           load_success[i] = library(packages[i]
+                           , character.only = TRUE
+                           , logical.return = TRUE
+                           )
+       }
+
+        if(min(load_success) == 0) {
+            warning("Unable to load all packages.")
+        } else {
+            return(1)
+        }
+    } else  {
+        warning("Packages not loaded.")
+        return(5)
+    }
 }
+
+
+
+## =============================================================================
+## Load Packages
+##
+## Loads all packages required by the project.
+## =============================================================================
+package_results = load_packages(packages = packages, install_missing = TRUE)
+if(package_results != 1) {
+    message("Attempted to load the following packages:")
+    message(packages)
+    stop("Package error.")
+}
+
+rm(load_packages)
+rm(package_results)
